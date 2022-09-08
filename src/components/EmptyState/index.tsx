71aa6c6852007ffe1b88950b89bf8767b9a6481c
arindampradhan <arindampradhan10@gmail.com>
2022-09-08 16:20:16 +0530

    fix: add emptystate component

---
+++ src/components/EmptyState/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Button from '../Button';
+
+type EmptyStateProps = {
+  header?: string;
+  description?: string;
+  primaryAction?: React.ReactNode;
+  secondaryAction?: React.ReactNode;
+  tertiaryAction?: React.ReactNode;
+  isLoading?: boolean;
+  imageUrl?: string;
+  customImage?: Function;
+  maxImageHeight?: number;
+  maxImageWidth?: number;
+  width?: 'narrow' | 'wide';
+};
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+  header,
+  description,
+  primaryAction,
+  secondaryAction,
+  tertiaryAction,
+  isLoading,
+  imageUrl,
+  customImage,
+  maxImageHeight = 160,
+  maxImageWidth = 160,
+  width = 'wide'
+}) => {
+  const widthBehaviour = {
+    wide: 'max-w-md',
+    narrow: 'max-w-xs'
+  };
+  return (
+    <div className={`mx-auto my-12 text-center ${widthBehaviour[width]}`}>
+      {customImage || (
+        <img
+          src={imageUrl}
+          style={{
+            maxWidth: `${maxImageWidth}px`,
+            maxHeight: `${maxImageHeight}px`
+          }}
+          className="mx-auto mt-0 mb-6"
+          alt="preview-state"
+          role="presentation"
+        />
+      )}
+      <h4 className="mb-4 text-xl leading-6 text-primarydark">{header}</h4>
+      <p className="mb-6 mt-0 text-primarydark">{description}</p>
+      <div className="mb-2 flex items-center justify-center gap-1">
+        {primaryAction}
+        {secondaryAction}
+        {isLoading && <span className="ml-4">o</span>}
+      </div>
+      {tertiaryAction}
+    </div>
+  );
+};
+
+export default EmptyState;
--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Background from '../components/Background';
-import Banner from '../components/Banner';
+import EmptyState from '../components/EmptyState';
+import Button from '../components/Button';
 
 const DesignSystem = () => {
   return (
-    <div className="bg-white">
+    <div>
       <Background>
-        <Banner appearance="error">
-          Payment details needed. To stay on your current plan after your trial
-          ends, add payment details by June 30, 2020.
-          <a href="/components/banner/examples">Add payment details</a>
-        </Banner>
+        <EmptyState
+          header="You don't have access to this issue"
+          description="Make sure the issue exists in this project. If it does, ask a project admin for permission to see the project's issues."
+          primaryAction={<Button>View permission</Button>}
+          tertiaryAction={<Button appearance="link">Learn more</Button>}
+          imageUrl="https://atlassian.design/static/LockClosed-18281e85ee8523db807bb04889ceaac8.png"
+          width="narrow"
+        />
       </Background>
     </div>
   );
